e2a7e599bff1f94f735edbf0021e4700a3270acf
Shaun Carland <shaun@Shauns-MacBook-Pro-2.local>
2020-03-19 17:39:26 -0400

    Add endpoint

--- app.rb
+++ app.rb
@@ -5,8 +5,22 @@ require_relative "person"
 require_relative "model"
 
 before do
-    request.body.rewind
-    @request_payload = JSON.parse(request.body.read)
+    # request.body.rewind
+    # @request_payload = JSON.parse(request.body.read)
+end
+
+get '/v1/sir_model' do
+    model = Model.new(
+        eons: params["eons"].to_i,
+        susceptible: params["susceptible"].to_i,
+        infected: params["infected"].to_i,
+        resistant: params["resistant"].to_i,
+        rate_si: params["rate_si"].to_f,
+        rate_ir: params["rate_ir"].to_f
+    )
+
+    content_type :json
+    { results: model.results }.to_json
 end
 
 post '/v1/policy_prices' do
@@ -26,3 +40,5 @@ post '/v1/policy_prices' do
     content_type :json
     { policy_prices: policy_prices }.to_json
 end
+
+# ?eons=5&susceptible=10&infected=0&resistant=0&rate_si=0.01&rate_ir=0.05
